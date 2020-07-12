1ed2d9bb3b516a09d71de03613a23de9c5a538ea
DavidCary <no-email>
2020-07-12 13:48:00 -0700

    Remove header elements from demos

--- docs/demo1/src/App.js
+++ docs/demo1/src/App.js
@@ -6,11 +6,6 @@ import QuoteGrid from './QuoteGrid.js';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Paired Quotes grid with variable height rows.
-        </p>
-      </header>
       <nav>
         <div><a href='../..'>react-measured-grid API</a></div>
         <div><a href='https://github.com/DavidCary/react-measured-grid'>repo</a></div>
